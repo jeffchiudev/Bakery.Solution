ecbeef16ce0f6734b4c21640602b6301382d01b7
Jeff Chiu <jwchiu82@gmail.com>
2020-12-11 10:36:42 -0800

    add nothing, still trying to figure stuff out

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using Bakery.Models
+using Bakery.Models;
 
 
 namespace Bakery.Tests
@@ -10,10 +10,11 @@ namespace Bakery.Tests
     public class POSLogicTests
     {
         [TestMethod]
-        public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+        public void Bread_MakeABreadOrder_Constructor()
         {
-        // any necessary logic to prep for test; instantiating new classes, etc.
-        Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+            Bread testBreadOrder = new Bread();
+            Assert.AreEqual(true, testBreadOrder.)
+
         }    
     }
 }
