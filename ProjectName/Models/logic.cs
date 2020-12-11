eb50e66c934d6df4328b6e32e4450992bfb50e12
jeffchiudev <jwchiu82@gmail.com>
2020-12-11 08:15:51 -0800

    Initial commit

---
+++ ProjectName/Models/logic.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace TODOProjectName.Models
+{
+    public class TODO
+    {
+
+    }
+}
+
+//BL goes here
---
+++ ProjectName/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace ScrabbleScore.Models
+{
+    public class Program
+    {
+        public static void Main()
+        {
+           
+
+        }
+    }
+}
